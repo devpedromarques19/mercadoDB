44b6ccf6967f4ca7b9723476a0965a3fed69e6b4
devpedromarques19 <dev.pedromarques@gmail.com>
2021-06-16 00:25:03 -0300

    Versão 1

---
+++ src/main/java/com/example/PessoaDB/api/PessoaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.PessoaDB.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PessoaDB/api/PessoaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PessoaDB.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
